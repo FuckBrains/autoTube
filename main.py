bc4fcfadc6166f4bbc04b896705dcff8e9022b52
chuski1212 <chuski1212@gmail.com>
2018-09-09 20:59:20 +0200

    adding schedule

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import schedule
+import time
 from twitch_clips import get_clips_by_lang
 from video_edition import edit_video
 from youtube import upload_video
 from cleaning import clean_files
 
-clips, first_title = get_clips_by_lang('all')
-edit_video(clips)
-upload_video('title test')
-clean_files()
 
+def job():
+    clips, first_title = get_clips_by_lang('all')
+    edit_video(clips)
+    upload_video('title test')
+    clean_files()
 
+schedule.every().day.at("15:30").do(job)
 
+while True:
+    schedule.run_pending()
+    time.sleep(1)
