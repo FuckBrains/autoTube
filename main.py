40aec0fa16027eb7622ce640619b9a86d46f3aeb
chuski1212 <chuski1212@gmail.com>
2018-09-10 02:02:44 +0200

    main

--- main.py
+++ main.py
@@ -6,14 +6,8 @@ from youtube import upload_video
 from cleaning import clean_files
 
 
-def job():
-    clips, first_title = get_clips_by_lang('all')
-    edit_video(clips)
-    upload_video('title test')
-    clean_files()
+clips, first_title = get_clips_by_lang('all')
+edit_video(clips)
+upload_video('title test')
+clean_files()
 
-schedule.every().day.at("15:30").do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
