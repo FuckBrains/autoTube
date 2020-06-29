8c22f892ff80059883f4e74dc4aa1a5e6b9feee0
chuski1212 <chuski1212@gmail.com>
2020-06-29 20:59:05 +0200

    testing_video

--- test.py
+++ test.py
@@ -3,14 +3,14 @@ import settings
 import sqlite3
 from repository import get_game_current_number, update_game_current_number
 
-# game = settings.GAMES['lol_es']
-# title = 'Test title'
-# file_path = '/home/jorge/IdeaProjects/autoTube/test_video.mp4'
-# description = 'test description'
-# category = 20
-# tags = ['test tag']
+game = settings.GAMES['lol_es']
+title = 'Test title'
+file_path = '/home/jorge/IdeaProjects/autoTube/test_video.mp4'
+description = 'test description'
+category = 20
+tags = ['test tag']
 
-# upload_video_to_youtube(game, title, file_path, description, category, tags)
+upload_video_to_youtube(game, title, file_path, description, category, tags)
 
 # conn = sqlite3.connect('database.db')
 # sql = ''' INSERT INTO video_numbers(game_key,current_number)
@@ -22,5 +22,5 @@ from repository import get_game_current_number, update_game_current_number
 # conn.close()
 
 # update_game_current_number('fortnite_en', 185)
-current_number = get_game_current_number('fortnite_en')
-print(current_number)
+# current_number = get_game_current_number('fortnite_en')
+# print(current_number)
