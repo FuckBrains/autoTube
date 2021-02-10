0ecb57c48808f1d3cd1dc92598825f326d95ebd8
chuski1212 <chuski1212@gmail.com>
2021-02-10 01:33:01 +0100

    update test.py

--- test.py
+++ test.py
@@ -3,23 +3,23 @@ import settings
 import sqlite3
 from repository import get_game_current_number, update_game_current_number
 
-game = settings.GAMES['lol_es']
-title = 'Test title'
-file_path = settings.RESULT_DIRECTORY + 'test_video.mp4'
-description = 'test description'
-category = 20
-tags = ['test tag']
+# game = settings.GAMES['lol_es']
+# title = 'Test title'
+# file_path = settings.RESULT_DIRECTORY + 'test_video.mp4'
+# description = 'test description'
+# category = 20
+# tags = ['test tag']
 
-upload_video_to_youtube(game, title, file_path, description, category, tags)
+# upload_video_to_youtube(game, title, file_path, description, category, tags)
 
-# conn = sqlite3.connect('database.db')
-# sql = ''' INSERT INTO video_numbers(game_key,current_number)
-#               VALUES(?,?) '''
-# c = conn.cursor()
-# task1 = ('fortnite_en', 180)
-# c.execute(sql, task1)
-# conn.commit()
-# conn.close()
+conn = sqlite3.connect('database.db')
+sql = ''' INSERT INTO video_numbers(game_key,current_number)
+              VALUES(?,?) '''
+c = conn.cursor()
+task1 = ('ark_es', 0)
+c.execute(sql, task1)
+conn.commit()
+conn.close()
 
 # update_game_current_number('fortnite_en', 185)
 # current_number = get_game_current_number('fortnite_en')
