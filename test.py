04d55ae7ed1cb220cd83fc5c85c8ddd034fef631
chuski1212 <chuski1212@gmail.com>
2021-03-14 16:09:33 +0100

    move thumbnail credentials to arkes

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ from thumbnail_generator_v2 import generate_thumbnail
 # update_game_current_number('ark_es', 4)
 # current_number = get_game_current_number('fortnite_en')
 # print(current_number)
-game = game_config.GAMES['chatting_es']
+game = game_config.GAMES['ark_es']
 # upload_video(game, 'elmillor lo da todo', ['elmillor', 'coscu'])
 # upload_video_api(game, 'test title', file_path, 'test description', 20, ['test tag'])
 thumbnail_url = get_video_thumbnail('8Kl0FmlagF0')
