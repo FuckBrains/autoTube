5cecbeae1c3f4c36b35ff605092e152d8edb8ce3
chuski1212 <chuski1212@gmail.com>
2021-04-10 13:26:04 +0200

    insert video number into db if key video number is not found

--- repository.py
+++ repository.py
@@ -13,6 +13,16 @@ def update_game_current_number(game_key, current_number):
     conn.commit()
     conn.close()
 
+def init_game_number(game_key):
+    conn = sqlite3.connect(settings.DATABASE_PATH)
+    sql = ''' INSERT INTO video_numbers(game_key,current_number) VALUES(?,?) '''
+    c = conn.cursor()
+    task1 = (game_key, 1)
+    c.execute(sql, task1)
+    conn.commit()
+    conn.close()
+
+
 def get_game_current_number(game_key):
     conn = sqlite3.connect(settings.DATABASE_PATH)
     sql = ''' SELECT current_number
@@ -23,4 +33,8 @@ def get_game_current_number(game_key):
     c.execute(sql, task)
     row = c.fetchone()
     conn.close()
-    return row[0]            
+    if row:
+        return row[0]
+    else:
+        init_game_number(game_key)
+        return 1
